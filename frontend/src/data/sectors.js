58955fb845e7baf89b6c97a75a8fc2a33aa714a2
TetaCindy <c.teta@alustudent.com>
2026-03-21 23:57:42 +0200

    8

---
+++ frontend/src/data/sectors.js
@@ -0,0 +1,39 @@
+// All 30 Rwanda districts with their sectors
+export const sectorsByDistrict = {
+  "Gasabo": ["Bumbogo","Gatsata","Gikomero","Gisozi","Jabana","Jali","Kacyiru","Kimihurura","Kimironko","Kinyinya","Ndera","Nduba","Remera","Rusororo","Rutunga"],
+  "Kicukiro": ["Gahanga","Gatenga","Gikondo","Kagarama","Kanombe","Kicukiro","Kigarama","Masaka","Niboye","Nyarugunga"],
+  "Nyarugenge": ["Gitega","Kanyinya","Kigali","Kimisagara","Mageragere","Muhima","Nyakabanda","Nyamirambo","Nyarugenge","Rwezamenyo"],
+  "Bugesera": ["Gashora","Juru","Kamabuye","Mareba","Mayange","Musenyi","Mwogo","Ngeruka","Ntarama","Nyamata","Rilima","Ruhuha","Rweru","Shyara"],
+  "Gatsibo": ["Gasange","Gatsibo","Gitoki","Kabarore","Kageyo","Kiramuruzi","Kiziguro","Muhura","Murambi","Ngarama","Nyagihanga","Remera","Rugarama"],
+  "Kayonza": ["Gahini","Kabare","Kabarondo","Mukarange","Murama","Murundi","Mwiri","Ndego","Nyamirama","Rukara","Ruramira","Rwinkwavu"],
+  "Kirehe": ["Gahara","Gatore","Kigarama","Kigina","Kirehe","Mahama","Mpanga","Musaza","Mushikiri","Nasho","Nyamugari","Nyarubuye"],
+  "Ngoma": ["Gashanda","Jarama","Karembo","Kibungo","Mugesera","Murama","Mutenderi","Remera","Rukira","Rukumberi","Rurenge","Sake"],
+  "Nyagatare": ["Gatunda","Karama","Karangazi","Katabagemu","Kiyombe","Matimba","Mimuri","Mukama","Musheli","Nyagatare","Rukomo","Rwempasha","Tabagwe"],
+  "Rwamagana": ["Fumbwe","Gahengeri","Gishari","Karenge","Kigabiro","Muhazi","Munyaga","Munyiginya","Musha","Muyumbu","Nzige","Rubona"],
+  "Burera": ["Bungwe","Butaro","Cyanika","Cyeru","Gahunga","Gatebe","Gitovu","Kagogo","Kinoni","Kinyababa","Kivuye","Nemba","Rugarama"],
+  "Gakenke": ["Busengo","Coko","Cyabingo","Gakenke","Gashenyi","Janja","Kamubuga","Karambo","Kivuruga","Mataba","Minazi","Muhondo","Muzo","Ruli","Rushashi"],
+  "Gicumbi": ["Bukure","Bwisige","Byumba","Cyumba","Giti","Kageyo","Kaniga","Manyagiro","Miyove","Mukarange","Muko","Mutete","Nyankenke","Rubaya","Shangasha"],
+  "Musanze": ["Busogo","Cyuve","Gacaca","Gashaki","Gataraga","Kimonyi","Kinigi","Muhoza","Muko","Musanze","Nkotsi","Nyange","Remera","Rwaza","Shingiro"],
+  "Rulindo": ["Base","Burega","Bushoki","Buyoga","Cyinzuzi","Cyungo","Kinihira","Kisaro","Masoro","Mbogo","Murambi","Ngoma","Ntarabana","Rukozo","Rusiga","Shyorongi"],
+  "Gisagara": ["Gikonko","Gishubi","Kansi","Kibilizi","Kigembe","Mamba","Muganza","Mugombwa","Mukingo","Musha","Ndora","Save"],
+  "Huye": ["Gishamvu","Karama","Kigoma","Kinazi","Maraba","Mbazi","Mukura","Ngoma","Ruhashya","Rusatira","Rwaniro","Simbi","Tumba"],
+  "Kamonyi": ["Gacurabwenge","Karama","Kayenzi","Kayumbu","Mugina","Musambira","Ngamba","Nyamiyaga","Nyarubaka","Rugalika","Rukoma","Runda"],
+  "Muhanga": ["Cyeza","Kabacuzi","Kibangu","Kiyumba","Muhanga","Mushishiro","Nyabinoni","Nyamabuye","Nyarusange","Rongi","Rugendabari","Shyogwe"],
+  "Nyamagabe": ["Buruhukiro","Cyanika","Gasaka","Gatare","Kaduha","Kamegeri","Kibirizi","Kitabi","Mbazi","Mugano","Musange","Musebeya","Nkomane","Tare"],
+  "Nyanza": ["Busasamana","Busoro","Cyabakamyi","Kibilizi","Kigoma","Mukingo","Muyira","Ntyazo","Nyagisozi","Rwabicuma"],
+  "Nyaruguru": ["Busanze","Cyahinda","Kibeho","Kivu","Mata","Muganza","Munini","Ngera","Ngoma","Nyabimata","Ruheru","Ruramba"],
+  "Ruhango": ["Bweramana","Byimana","Kabagari","Kinazi","Kinihira","Mbuye","Mwendo","Ntongwe","Ruhango"],
+  "Karongi": ["Bwishyura","Gashari","Gishyita","Gitesi","Mubuga","Murambi","Murundi","Mutuntu","Rubengera","Ruganda","Rwankuba","Twumba"],
+  "Ngororero": ["Bwira","Gatumba","Hindiro","Kabaya","Kageyo","Kavumu","Matyazo","Muhanda","Muhororo","Ndaro","Ngororero","Nyange","Sovu"],
+  "Nyabihu": ["Bigogwe","Jenda","Jomba","Kabatwa","Karago","Kintobo","Mukamira","Muringa","Rambura","Rugera","Rurembo","Shyira"],
+  "Nyamasheke": ["Bushekeri","Bushenge","Cyato","Gihombo","Kagano","Kanjongo","Karambi","Karengera","Kirimbi","Macuba","Mahembe","Rangiro","Shangi"],
+  "Rubavu": ["Bugeshi","Busasamana","Cyanzarwe","Gisenyi","Kanama","Kanzenze","Mudende","Nyakiliba","Nyamyumba","Nyundo","Rubavu","Rugerero"],
+  "Rusizi": ["Bugarama","Butare","Bweyeye","Gashonga","Giheke","Gihundwe","Kamembe","Muganza","Mururu","Nkanka","Nkombo","Nkungu","Nyakabuye","Nzahaha"],
+  "Rutsiro": ["Boneza","Gihango","Kigeyo","Kivumu","Manihira","Mukura","Murunda","Musasa","Mushonyi","Mushubati","Nyabirasi","Ruhango","Rusebeya"],
+};
+
+export const allSectors = Object.entries(sectorsByDistrict)
+  .flatMap(([district, sectors]) => sectors.map(s => `${s}, ${district}`))
+  .sort();
+
+export const allDistricts = Object.keys(sectorsByDistrict).sort();
